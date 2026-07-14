a95c751f593656ce0bbb70b45ae40234eaece5a2
granat <alva@dehbeanpc>
2026-07-14 19:23:11 +0200

    kernel panic thingamajig

--- clickto10.js
+++ clickto10.js
@@ -3,18 +3,28 @@ const counter = document.getElementById("counter")
 function handleClick() {
     let value = parseInt(counter.textContent);
     let thing = document.getElementById("thing");
-    value++;
-    counter.textContent = value;
     if (value !== 0) {
         thing.style.display = "none";
     }
     if (value === 10) {
         ten();
     }
+    value++;
+    counter.textContent = value;
+}
+
+function kpanic() {
+    fetch("kpanic.html")
+        .then(response => response.text())
+        .then(newhtml =>{
+            document.documentElement.innerHTML = newhtml;
+            console.log("thy bsod happened")
+        })
 }
 
 function ten() {
-    document.removeEventListener("click", handleClick)
+    document.removeEventListener("click", handleClick);
+    kpanic();
 }
 
 document.addEventListener("click", handleClick)
